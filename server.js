9e99d5dbb9c34285cc65f607d84ce1bffcdd2851
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-05-12 17:18:24 -0700

    MVP has been met

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const cars = require('./cars/carsRouter');
 const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
 // server.use("/api/cars")
+server.use("/api/cars", cars);
 
 
 server.get("/", (req, res) => {
